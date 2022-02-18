5ee28f6474b117a35dbc82f34c2d153aaf1a8340
Karoline Nascimento França <karolinenascimento777@gmail.com>
2022-02-17 23:12:55 -0300

    criação do primeiro projeto

---
+++ MeuPrimeiroProjeto/MeuPrimeiroProjeto/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MeuPrimeiroProjeto
+//
+//  Created by Karoline Do Nascimento on 17/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
